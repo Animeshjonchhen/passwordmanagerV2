7a7d729fa06fbd41640ea42663896f73fcb2f391
anjon062 <animesh.jonchhen@javra.com>
2022-10-17 11:56:14 +0545

    added loginController

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Validation\ValidationException;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('Login.loginForm');
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function create()
+    {
+        $credentials = request()->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        if (!auth()->attempt($credentials)) {
+            throw ValidationException::withMessages([
+                'email' => 'Your provided credentials could not be verified'
+            ]);
+        }
+
+        return redirect('/');
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function destroy()
+    {
+        auth()->logout();       
+    }
+}
