acfaeb9aaf2aa67ad45e205938e811076e2819c0
anjon062 <animesh.jonchhen@javra.com>
2022-10-17 11:57:21 +0545

    added CategoryController

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+
     public function index()
     {
-
+        return view('Category.index',[
+            'categories' => Category::all()
+        ]);
     }
 
     public function show()
@@ -18,16 +22,46 @@ class CategoryController extends Controller
 
     public function create()
     {
-
+        return view('Category.create');
     }
 
-    public function edit()
+    public function store()
     {
+        $attributes = request()->validate([
+            'name' => 'required',
+        ]);
 
+        $attributes['user_id'] = auth()->user()->id;
+
+        Category::create($attributes);
+
+        return redirect('/categories');
     }
 
-    public function destroy()
+    public function edit(Category $category)
     {
-        
+        return view('Category.updateCategory',[
+            'category' => Category::find($category->id)
+        ]);
+    }
+
+    public function update(Category $category)
+    {
+        $attributes = request()->validate([
+            'name' => 'required|unique'
+        ]);
+
+        $attributes['user_id'] = auth()->user()->id;
+
+        $category->update($attributes);
+
+        return redirect('/categories');
+    }
+
+    public function destroy(Category $category)
+    {
+        $category->delete();
+
+        return redirect('/categories');
     }
 }
--- app/Models/Password.php
+++ app/Models/Password.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Password extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'category_id',
+        'email',
+        'password',
+        'url',
+        'remarks'
+    ];
 }
